57760fe2c3a1d50529f3d10659b1012eb38d38e7
Talita de Oliveira Ferreira <talita@gmail.com>
2015-12-13 22:23:37 -0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+# TOF - 13/12/2015
+dados<- read.table("household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
+
+# Subsetting
+dados<- subset(dados, (dados$Date == "1/2/2007" | dados$Date== "2/2/2007")) 
+
+# Plot1
+png("plot1.png", width=480, height= 480)
+
+hist(dados$Global_active_power, col= "red", xlab= "Global Active Power (kilowatts)", ylab= "Frequency", main= "Global Active Power")
